ca8543b4528dee48b48178f3527ff53fee952f02
Juan Valencia <judvalenciaan@unal.edu.co>
2020-02-11 23:29:49 -0500

    Reducing number

--- download_data.py
+++ download_data.py
@@ -7,7 +7,7 @@ end = datetime.now()
 
 symbols_list = ['ORCL', 'TSLA', 'IBM','YELP', 'MSFT']
 with open('sp500list', 'r') as fi:
-    symbols_list = list(set(fi.read().split()))[:200]
+    symbols_list = list(set(fi.read().split()))[:100]
     
 
 print(symbols_list)
